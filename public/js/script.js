f8f0926d2e6722f75aa0d5989e1f5bdba0c4fc64
Rodrigo Marí <rorro2001@gmail.com>
2025-11-04 20:43:54 -0300

    Fixed: script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // URL de tu API local
-  const API_URL = "http://127.0.0.1:5000";
+  const API_URL = "";
   const VIDAS_INICIALES = 6;
 
   // Elementos del DOM
