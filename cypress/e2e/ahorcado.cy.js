df5a710f530efa812890ecbaab1d6f32185e3691
Rodrigo Marí <rorro2001@gmail.com>
2025-11-10 21:40:06 -0300

    Changed: ATDD para que falle 2

--- cypress/e2e/ahorcado.cy.js
+++ cypress/e2e/ahorcado.cy.js
@@ -36,7 +36,7 @@ describe("Juego del Ahorcado - Flujos E2E", () => {
         case "p":
           req.reply({
             body: {
-              palabra_oculta: ["p", "e", "_", "_", "_", "_"],
+              palabra_oculta: ["p", "_", "_", "_", "_", "_"],
               vidas: VIDAS_INICIALES,
               letras_intentadas: ["p"],
               terminado: false,
@@ -47,7 +47,7 @@ describe("Juego del Ahorcado - Flujos E2E", () => {
         case "y":
           req.reply({
             body: {
-              palabra_oculta: ["p", "y", "_", "_", "_", "_"],
+              palabra_oculta: ["p", "y", "e", "_", "_", "_"],
               vidas: VIDAS_INICIALES,
               letras_intentadas: ["p", "y"],
               terminado: false,
